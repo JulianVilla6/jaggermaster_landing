1e9c7ec8912303eff715409a72d915f72414da8a
Julian Villamizar <julianvillamizarhidalgo@gmail.com>
2017-02-12 12:54:54 -0500

    video correciones

--- js/anchors.js
+++ js/anchors.js
@@ -19,3 +19,13 @@ $(window).scroll(function() {
       $('.anchorLink2').fadeOut(500);
      }
  });
+ var myVideo = document.getElementById('index-video');
+ if (typeof myVideo.loop == 'boolean') { // loop supported
+     myVideo.loop = true;
+ } else { // loop property not supported
+     myVideo.on('ended', function () {
+     this.currentTime = 0;
+     this.play();
+     }, false);
+ }
+ myVideo.play();
